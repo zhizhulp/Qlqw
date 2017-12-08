4a1c824fe7f719a31cf7c64c51d815097b252409
admin <admin>
2017-12-08 16:09:58 +0800

    商户入驻接口

--- app/src/main/java/com/ascba/rebate/view/jd_selector/BottomDialog.java
+++ app/src/main/java/com/ascba/rebate/view/jd_selector/BottomDialog.java
@@ -3,6 +3,7 @@ package com.ascba.rebate.view.jd_selector;
 import android.app.Dialog;
 import android.content.Context;
 import android.view.Gravity;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -19,13 +20,18 @@ public class BottomDialog extends Dialog {
 
     private void init(Context context) {
         selector2 = new AddressSelector(context);
-        setContentView(selector2.getView());
-        setCancelable(false);
-        setCanceledOnTouchOutside(false);
+        View view = selector2.getView();
+        view.findViewById(R.id.im_close).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+        setContentView(view);
         Window window = getWindow();
         WindowManager.LayoutParams params = window.getAttributes();
         params.width = WindowManager.LayoutParams.MATCH_PARENT;
-        params.height = (int) ScreenDpiUtils.dp2px(context, 256);
+        params.height = (int) ScreenDpiUtils.dp2px(context, 310);
         window.setAttributes(params);
 
         window.setGravity(Gravity.BOTTOM);
